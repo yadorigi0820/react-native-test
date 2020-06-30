402855c6e59f7c32406f587e1af73b749cddda13
yadorigi0820 <yadorigi0820@gmail.com>
2020-06-30 23:26:57 +0900

    ボタンを付けた

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
+  const [number, setNumber] = useState(1);
+  function test() {
+    setNumber(number + 1);
+    console.log(`現在の数字: ${number}`);
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>現在の数字: {number}</Text>
+      <Button
+        onPress={() => {
+          test();
+        }}
+        title="ボタン"
+      ></Button>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +25,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
